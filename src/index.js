94d15e99c9cc0ff0c0d1c7532e6ebc15be6cb26a
YuriBSSE <ahsanmuneer81@gmail.com>
2022-10-05 11:12:54 +0500

    complete

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const javscript = () => {
   //Q1   Install Node.js and VS Code on your computer.
   // Completed
 
-  //Q2 Personal Message: Store a personâ€™s name in a variable, and print a message to that person. Your message should be simple, such as, â€œHello Eric, would you like to learn some Python today?â€
+  //Q2
   var person = "Hello Eric, would you like to learn some Python today?";
   console.log(person);
 
-  //Q3 Name Cases: Store a personâ€™s name in a variable, and then print that personâ€™s name in lowercase, uppercase, and titlecase.
+  //Q3
   // * LowerCase
   let texts = "Hello World!";
   let results = texts.toLowerCase();
@@ -32,24 +32,23 @@ const javscript = () => {
   vfinal = vfinal.substring(0, vfinal.length - 1);
   console.log(vfinal);
 
-  //Q4  Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-  // Albert Einstein once said, â€œA person who never made a mistake never tried anything new.â€
+  //Q4
   var quote =
     "Albert Einstein once said, â€œA person who never made a mistake never tried anything new.â€";
   console.log(quote);
 
-  // Q5 Famous Quote 2: Repeat Exercise 4, but this time store the famous personâ€™s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+  // Q5
   var famous_person = "Albert Einstein ";
   var famous_quotes =
     "once said, â€œA person who never made a mistake never tried anything new.â€";
   console.log(famous_person + famous_quotes); //Q10: this programme add 2 variables and return concatinated value
 
-  //Q6 Stripping Names: Store a personâ€™s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+  //Q6
 
   var name = " \tJames Bond\n ";
   console.log(name.trim()); //Q10: this programme removes speace from string and return trimmed value
 
-  // Q7 Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+  // Q7
   console.log(5 + 3);
   console.log(12 - 4);
   console.log(4 * 2);
@@ -57,6 +56,9 @@ const javscript = () => {
 
   //Q8 You should create four lines that look like this:
 
+  console.log("===================================================");
+  console.log(5 + 3);
+  console.log("===================================================");
   //Q9 Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
   var favNum = 8;
   console.log("my favourite number is", favNum);
@@ -64,20 +66,25 @@ const javscript = () => {
   //Q10 Adding Comments: Choose two of the programs youâ€™ve written, and add at least one comment to each. If you donâ€™t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
   // ANSWER 6 ND 5
 
-  //Q11 Names: Store the names of a few of your friends in a array called names. Print each personâ€™s name by accessing each element in the list, one at a time.
+  //Q11
+
+  const nnnn = ["muhammad", "ahsan", "muneer"];
+  console.log(nnnn[0]);
+  console.log(nnnn[1]);
+  console.log(nnnn[2]);
 
   var arrFriends = ["muhammad", "ahsan", "muneer"];
   arrFriends.forEach((name) => {
     console.log(name);
   });
 
-  //Q12 Greetings: Start with the array you used in Exercise 11, but instead of just printing each personâ€™s name, print a message to them. The text of each message should be the same, but each message should be personalized with the personâ€™s name.
+  //Q12
   var arrfriendStatements = ["muhammad", "ahsan", "muneer"];
   arrfriendStatements.forEach((nameState) => {
     console.log("helow my friend", nameState);
   });
 
-  // Q13 Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as â€œI would like to own a Honda motorcycle.â€
+  // Q13
 
   var arrTransports = [
     "I love to ride Honda Civic",
@@ -88,7 +95,7 @@ const javscript = () => {
     console.log(transport);
   });
 
-  // Q 14 Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people youâ€™d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+  // Q 14
   var arrPeople = ["muhammad", "ahsan", "muneer"];
   arrPeople.forEach((guests) => {
     console.log(
@@ -97,10 +104,7 @@ const javscript = () => {
     );
   });
 
-  // Q 15 Changing Guest List: You just heard that one of your guests canâ€™t make the dinner, so you need to send out a new set of invitations. Youâ€™ll have to think of someone else to invite.
-  // Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who canâ€™t make it.
-  // â€¢ Modify your list, replacing the name of the guest who canâ€™t make it with the name of the new person you are inviting.
-  // â€¢ Print a second set of invitation messages, one for each person who is still in your list.
+  // Q 15
   var arrGuests = ["muhammad", "ahsan", "muneer"];
   arrGuests[2] = "text";
   arrGuests.forEach((guests) => {
@@ -109,10 +113,7 @@ const javscript = () => {
       guests
     );
   });
-  //    Q16 More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
-  // â€¢ Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
-  // â€¢ Add one new guest to the beginning of your array.
-  // â€¢ Add one new guest to the middle of your array. â€¢ Use append() to add one new guest to the end of your list. â€¢ Print a new set of invitation messages, one for each person in your list.
+  // Q16
 
   var arrGuestsAdded = ["muhammad", "ahsan", "muneer"];
   arrGuestsAdded.unshift("muhammad");
@@ -126,14 +127,7 @@ const javscript = () => {
     );
   });
 
-  // Q17 Shrinking Guest List: You just found out that your new dinner table wonâ€™t arrive in time for the dinner, and you have space for only two guests.
-  // â€¢ Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite only two people for dinner.
-
-  // â€¢ Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know youâ€™re sorry you canâ€™t invite them to dinner.
-
-  // â€¢ Print a message to each of the two people still on your list, letting them know theyâ€™re still invited.
-
-  // â€¢ Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+  // Q17
   for (let i = arrGuestsAdded.length - 1; i >= 2; i--) {
     poppedPerson = arrGuestsAdded.pop();
     console.log(
@@ -147,33 +141,32 @@ const javscript = () => {
     console.log("You are still invited over dinner, dont worry! hehe", guests);
   });
 
-  //Q18 Seeing the World: Think of at least five places in the world youâ€™d like to visit.
-  // â€¢ Store the locations in a array. Make sure the array is not in alphabetical order.
+  //Q18
   var original = ["pakistan", "south africa", "dubai", "england", "california"];
 
-  // â€¢ Print your array in its original order.
+  //  Print your array in its original order.
   console.log(original);
-  // â€¢ Print your array in alphabetical order without modifying the actual list.
+  //  Print your array in alphabetical order without modifying the actual list.
   console.log([...original].sort());
-  // â€¢ Show that your array is still in its original order by printing it.
+  //  Show that your array is still in its original order by printing it.
   console.log(original);
-  // â€¢ Print your array in reverse alphabetical order without changing the order of the original list.
+  //  Print your array in reverse alphabetical order without changing the order of the original list.
   console.log([...original].sort().reverse());
-  // â€¢ Show that your array is still in its original order by printing it again.
+  //  Show that your array is still in its original order by printing it again.
   console.log(original);
-  // â€¢ Reverse the order of your list. Print the array to show that its order has changed.
+  //  Reverse the order of your list. Print the array to show that its order has changed.
   console.log(original.reverse());
-  // â€¢ Reverse the order of your list again. Print the list to show itâ€™s back to its original order.
+  //  Reverse the order of your list again. Print the list to show itâ€™s back to its original order.
   console.log(original.reverse());
-  // â€¢ Sort your array so itâ€™s stored in alphabetical order. Print the array to show that its order has been changed.
+  //  Sort your array so itâ€™s stored in alphabetical order. Print the array to show that its order has been changed.
   console.log(original.sort());
-  // â€¢ Sort to change your array so itâ€™s stored in reverse alphabetical order. Print the list to show that its order has changed.
+  //  Sort to change your array so itâ€™s stored in reverse alphabetical order. Print the list to show that its order has changed.
   console.log(original.sort().reverse());
 
-  //Q19 Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+  //Q19
   console.log(arrPeople.length);
 
-  //Q20 Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else youâ€™d like. Write a program that creates a list containing these items.
+  //Q20
 
   var x = [];
   function myFunction(a) {
@@ -183,27 +176,17 @@ const javscript = () => {
   }
   myFunction("abcd");
 
-  //Q21 They think of something you could store in a JavaScript Object. Write a program that creates Objects containing these items.
+  //Q21
 
   const car = { type: "Fiat", model: "500", color: "white" };
 
   console.log(car);
 
-  //Q22 Intentional Error: If you havenâ€™t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+  //Q22
   var y = [4, 3, 2, 1];
   console.log(y[6]);
 
-  //Q23 Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
-
-  // let car = 'subaru';
-
-  // console.log("Is car == 'subaru'? I predict True.")
-
-  // console.log(car == 'subaru')
-
-  // â€¢ Look closely at your results, and make sure you understand why each line evaluates to True or False.
-
-  // â€¢ Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+  //Q23
   const cat = "meaw";
 
   console.log(cat == "meaw");
@@ -249,18 +232,7 @@ const javscript = () => {
 
   console.log(!cat5 == "meaw");
 
-  // Q24 More Conditional Tests: You donâ€™t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
-  // â€¢ Tests for equality and inequality with strings
-
-  // â€¢ Tests using the lower case function
-
-  // â€¢ Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
-
-  // â€¢ Tests using "and" and "or" operators
-
-  // â€¢ Test whether an item is in a array
-
-  // â€¢ Test whether an item is not in a array
+  // Q24
 
   const a = "Hello";
   const b = "ahsan";
@@ -272,7 +244,7 @@ const javscript = () => {
   let res = Array.isArray(b);
   console.log("a is Array", res);
 
-  // 25. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+  // 25.
 
   let alien_color = "red";
 
@@ -282,7 +254,7 @@ const javscript = () => {
 
   console.log(alien_color == "blue" && "OK");
 
-  // 26. Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+  // 26.
 
   let alien = "red";
   console.log(alien == "red" ? true : false);
@@ -290,7 +262,7 @@ const javscript = () => {
   alien = "blue";
   console.log(alien == "blue" ? true : false);
 
-  // 27. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+  // 27.
 
   let a1 = "hydrabad";
   if (a1 == "asassa") {
@@ -321,7 +293,7 @@ const javscript = () => {
     console.log("else");
   }
 
-  // 28. Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+  // 28.
 
   let age = 25;
   if (age <= 2) {
@@ -338,9 +310,7 @@ const javscript = () => {
     console.log("expire XD");
   }
 
-  // 29. Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements
-  //   that check for certain fruits in your array.
-  // Make a array of your three favorite fruits and call it favorite_fruits.
+  // 29.
 
   let fruits = ["kinno", "saib", "Keela"];
   for (let i = 0; i < fruits.length; i++) {
@@ -353,8 +323,7 @@ const javscript = () => {
     }
   }
 
-  // 30. Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a
-  // greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+  // 30.
 
   let namesArray = [
     "Admin",
@@ -371,7 +340,7 @@ const javscript = () => {
       console.log(`Hello  ${namesArray[i]} , logging in again`);
   }
 
-  // 31. No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+  // 31.
 
   var newArray = [];
   if (newArray.length !== 0) {
@@ -384,7 +353,7 @@ const javscript = () => {
     console.log("We need to find some users!");
   }
 
-  // 32. Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+  // 32.
 
   namesArray.map((item) => item.toLowerCase());
   namesArray.map((item) => item.toLowerCase());
@@ -397,7 +366,7 @@ const javscript = () => {
     }
   }
 
-  // 33. Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+  // 33.
 
   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   for (let i = 0; i < numbers.length; i++) {
@@ -413,19 +382,151 @@ const javscript = () => {
   }
 };
 
+// 35.
 
-// 35. Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
-
-let animalsList = ["Loin", "Horse", "Cheeta"];
-for (let i = 0; i < animalsList.length; i++) {
+var animalsList = ["Loin", "Horse", "Cheeta"];
+let i;
+for (i = 0; i < animalsList.length; i++) {
   if (animalsList[i] == "Loin") {
     console.log(animalsList[i]);
   } else if (animalsList[i] == "Horse") {
-    console.log(animalsList[i] );
+    console.log(animalsList[i]);
   } else {
     console.log("Sorry");
   }
-}
 
+  // 36.
+
+  function make_shirt(s, m) {
+    console.log(
+      `TShirt is ${s}, message that should be printed on the shirt ${m}`
+    );
+  }
+  make_shirt("sm", " Here we go again");
+
+  // 37.
+
+  function make_shirt1(s, m) {
+    console.log(
+      `TShirt is ${s}, message that should be printed on the shirt ${m}`
+    );
+  }
+  make_shirt1("sm", " I love JavaScript");
+  make_shirt1("md", " I love JavaScript");
+  make_shirt1("lg", " I love JavaScript");
+
+  // 38.
+
+  function describe_city(city, country) {
+    console.log(`${city} is in ${country}`);
+  }
+  describe_city("London", "UK");
+  describe_city("Karachi", "Pakistan");
+  describe_city("Dehli", "india");
+
+  // 39.
+
+  function city_country(city, country) {
+    return `${city} , ${country}`;
+  }
+  let lin = city_country("London", "UK");
+  console.log(lin);
+
+  // 40
+
+  function make_album(n, tit, tra) {
+    if (tra == null) {
+      let object;
+      object = {
+        name: n,
+        title: tit,
+      };
+      return object;
+    }
+    object = {
+      name: n,
+      title: tit,
+      no: tra,
+    };
+    return object;
+  }
+  let first = make_album("abc", "xyz");
+  let second = make_album("asd ", "asd", 2);
+  let third = make_album("asd", "ahsan", 1);
+  console.log(first);
+  console.log(second);
+  console.log(third);
+
+  // 41.
+
+  function show_magicians(name) {
+    console.log(name);
+  }
+  let arr1 = ["a", "b", "c", "d", "e"];
+  for (let i = 0; i < arr1.length; i++) {
+    show_magicians(arr1[i]);
+  }
+
+  // 42.
+
+  function show_magicians_agian(name) {
+    console.log(name);
+  }
+  function make_great(name) {
+    show_magicians_agian("Alphabets " + name);
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    make_great(arr1[i]);
+  }
+
+  // 43.
+
+  function make_great1(name) {
+    console.log(name);
+  }
+  function make_great1(name) {
+    show_magicians2("memebers " + name);
+  }
+  let arrayOfPMLN = ["Nawaz", "Shabaz", "Dar", "Maryam"];
+  for (let i = 0; i < arrayOfPMLN.length; i++) {
+    let newObj = arrayOfPMLN[i];
+    make_great1(newObj);
+  }
+  for (let i = 0; i < arrayOfPMLN.length; i++) {
+    let newObj = arrayOfPMLN[i];
+    show_magicians2(newObj);
+  }
+
+  function show_magicians2(name) {
+    console.log(name);
+  }
+
+  // 44.
+
+  function sandwich(a) {
+    console.log(a);
+  }
+  sandwich("a");
+  sandwich("v");
+  sandwich("c");
+
+  // 45.
+
+  function cars(m, n, c, f) {
+    let obj = {
+      assembler: m,
+      year: n,
+      color: c,
+      accentuated: f,
+    };
+    return obj;
+  }
+  let car1_Object = cars("mehran", "2012", "red", "automatic");
+  let car2_Object = cars("civic", "2013", "black", "new");
+
+  console.log(car1_Object);
+  console.log(car2_Object);
+}
 
 javscript();
